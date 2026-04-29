1c0c5c31878660e253f46831cc71f84cd83da6cd
Philip Meier <github.pmeier@posteo.de>
2026-04-29 10:45:03 +0200

    cleanup

--- scripts/api_e2e.py
+++ scripts/api_e2e.py
@@ -2,8 +2,10 @@ import contextlib
 import json
 import uuid
 
+import ag_ui.core
 import httpx
 import httpx_sse
+import pydantic
 
 
 def header(title: str):
@@ -23,9 +25,9 @@ def assert_successful_response(response: httpx.Response) -> httpx.Response:
     content = "<non-decodable bytes>"
     with contextlib.suppress(Exception):
         content = response.text
-        content = json.dumps(json.loads(content), indent=2)
+        content = json.loads(content)
 
-    raise AssertionError(f"request failed: url={response.url}, status_code={response.status_code}: {content}")
+    raise AssertionError(f"request failed: url={response.url}, status_code={response.status_code}, {content=}")
 
 
 def main():
@@ -42,17 +44,8 @@ def main():
 
     agent_ids = [a["id"] for a in config["agents"]]
 
-    input_content = assert_successful_response(
-        client.post(
-            "/api/files",
-            json={"type": "image", "source": {"type": "url", "value": "https://picsum.photos/200/300"}},
-        )
-    ).json()
-    print(json.dumps(input_content, indent=2))
-    file_id = input_content["source"]["value"]["fileId"]
-
-    content = assert_successful_response(client.get(f"/api/files/{file_id}/content")).content
-    print(b"".join([content[:3], f"<{len(content) - 6} bytes>".encode(), content[-3:]]))
+    header("delete existing threads")
+    assert_successful_response(client.request("DELETE", "/api/threads", json={}))
 
     header("new thread")
     thread = assert_successful_response(
@@ -65,8 +58,14 @@ def main():
     ).json()
     print(json.dumps(thread, indent=2))
 
-    header("new run ")
+    header("new run in same thread with frontend tool call")
 
+    class CheerInput(pydantic.BaseModel):
+        name: str = pydantic.Field(description="Name of the user")
+
+    tool_name: str = "cheer_up_user"
+    tool_call_id: str | None = None
+    tool_call_arguments_json_deltas: list[str] = []
     with httpx_sse.connect_sse(
         client,
         "POST",
@@ -76,10 +75,52 @@ def main():
                 {
                     "id": str(uuid.uuid4()),
                     "role": "user",
-                    "content": [
-                        {"type": "text", "text": "What can you see on the image?"},
-                        input_content,
-                    ],
+                    "content": "I'm Huginn and I'm feeling a little down today.",
+                },
+            ],
+            "tools": [
+                {
+                    "name": tool_name,
+                    "description": "Cheer up the user",
+                    "parameters": CheerInput.model_json_schema(),
+                },
+            ],
+        },
+    ) as event_source:
+        assert_successful_response(event_source.response)
+        for sse in event_source.iter_sse():
+            event = sse.json()
+            print(json.dumps(event, indent=2))
+            match event["type"]:
+                case "TOOL_CALL_START":
+                    assert event["toolCallName"] == tool_name
+                    tool_call_id = event["toolCallId"]
+                case "TOOL_CALL_ARGS":
+                    tool_call_arguments_json_deltas.append(event["delta"])
+                case "TOOL_CALL_END":
+                    assert event["toolCallId"] == tool_call_id
+
+    tool_call = ag_ui.core.ToolCall(
+        id=tool_call_id,
+        function=ag_ui.core.FunctionCall(name=tool_name, arguments="".join(tool_call_arguments_json_deltas)),
+    )
+    print(tool_call.model_dump_json(indent=2))
+
+    thread = assert_successful_response(client.get(f"/api/threads/{thread['id']}")).json()
+    print(json.dumps(thread, indent=2))
+
+    header("new run in same thread with frontend tool call result")
+    with httpx_sse.connect_sse(
+        client,
+        "POST",
+        f"/api/threads/{thread['id']}/run",
+        json={
+            "messages": [
+                {
+                    "id": str(uuid.uuid4()),
+                    "content": json.dumps({"successful": True}),
+                    "role": "tool",
+                    "toolCallId": tool_call_id,
                 },
             ],
         },
@@ -89,99 +130,12 @@ def main():
             event = sse.json()
             print(json.dumps(event, indent=2))
 
-    #
-    # header("delete existing threads")
-    # assert_successful_response(client.request("DELETE", "/api/threads", json={}))
-    #
-    # header("new thread")
-    # thread = assert_successful_response(
-    #     client.post(
-    #         "/api/threads",
-    #         json={
-    #             "agentId": agent_ids[0],
-    #         },
-    #     )
-    # ).json()
-    # print(json.dumps(thread, indent=2))
-    #
-    # header("new run in same thread with frontend tool call")
-    #
-    # class CheerInput(pydantic.BaseModel):
-    #     name: str = pydantic.Field(description="Name of the user")
-    #
-    # tool_name: str = "cheer_up_user"
-    # tool_call_id: str | None = None
-    # tool_call_arguments_json_deltas: list[str] = []
-    # with httpx_sse.connect_sse(
-    #     client,
-    #     "POST",
-    #     f"/api/threads/{thread['id']}/run",
-    #     json={
-    #         "messages": [
-    #             {
-    #                 "id": str(uuid.uuid4()),
-    #                 "role": "user",
-    #                 "content": "I'm Huginn and I'm feeling a little down today.",
-    #             },
-    #         ],
-    #         "tools": [
-    #             {
-    #                 "name": tool_name,
-    #                 "description": "Cheer up the user",
-    #                 "parameters": CheerInput.model_json_schema(),
-    #             },
-    #         ],
-    #     },
-    # ) as event_source:
-    #     assert_successful_response(event_source.response)
-    #     for sse in event_source.iter_sse():
-    #         event = sse.json()
-    #         print(json.dumps(event, indent=2))
-    #         match event["type"]:
-    #             case "TOOL_CALL_START":
-    #                 assert event["toolCallName"] == tool_name
-    #                 tool_call_id = event["toolCallId"]
-    #             case "TOOL_CALL_ARGS":
-    #                 tool_call_arguments_json_deltas.append(event["delta"])
-    #             case "TOOL_CALL_END":
-    #                 assert event["toolCallId"] == tool_call_id
-    #
-    # tool_call = ag_ui.core.ToolCall(
-    #     id=tool_call_id,
-    #     function=ag_ui.core.FunctionCall(name=tool_name, arguments="".join(tool_call_arguments_json_deltas)),
-    # )
-    # print(tool_call.model_dump_json(indent=2))
-    #
-    # thread = assert_successful_response(client.get(f"/api/threads/{thread['id']}")).json()
-    # print(json.dumps(thread, indent=2))
-    #
-    # header("new run in same thread with frontend tool call result")
-    # with httpx_sse.connect_sse(
-    #     client,
-    #     "POST",
-    #     f"/api/threads/{thread['id']}/run",
-    #     json={
-    #         "messages": [
-    #             {
-    #                 "id": str(uuid.uuid4()),
-    #                 "content": json.dumps({"successful": True}),
-    #                 "role": "tool",
-    #                 "toolCallId": tool_call_id,
-    #             },
-    #         ],
-    #     },
-    # ) as event_source:
-    #     assert_successful_response(event_source.response)
-    #     for sse in event_source.iter_sse():
-    #         event = sse.json()
-    #         print(json.dumps(event, indent=2))
-    #
-    # thread = assert_successful_response(client.get(f"/api/threads/{thread['id']}")).json()
-    # print(json.dumps(thread, indent=2))
-    #
-    # header("list threads")
-    # thread = assert_successful_response(client.get("/api/threads", params={"sortBy": "createdAt"})).json()
-    # print(json.dumps(thread, indent=2))
+    thread = assert_successful_response(client.get(f"/api/threads/{thread['id']}")).json()
+    print(json.dumps(thread, indent=2))
+
+    header("list threads")
+    thread = assert_successful_response(client.get("/api/threads", params={"sortBy": "createdAt"})).json()
+    print(json.dumps(thread, indent=2))
 
 
 if __name__ == "__main__":
