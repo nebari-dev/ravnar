7a6b2bb31700be42c713c4e8b9752c0dbb6ddad1
Philip Meier <github.pmeier@posteo.de>
2026-05-04 10:03:40 +0200

    fix mypy

--- src/_ravnar/file_storage.py
+++ src/_ravnar/file_storage.py
@@ -98,15 +98,13 @@ class FileHandler:
     async def add_or_read(
         self, file_input_content: schema.FileInputContent, *, user_id: str
     ) -> tuple[schema.RavnarFileInputContent, bytes]:
-        rfic: schema.RavnarFileInputContent
         if (
             isinstance(file_input_content.source, ag_ui_input_content_compat.InputContentCustomSource)
             and file_input_content.source.name == "ravnar"
         ):
-            rfic = pydantic.TypeAdapter(schema.RavnarFileInputContent).validate_python(
-                file_input_content, from_attributes=True
-            )
-            _, content = await self.read(rfic.source.value.file_id, user_id=user_id)  # type: ignore
+            ta = pydantic.TypeAdapter[schema.RavnarFileInputContent](schema.RavnarFileInputContent)
+            rfic = ta.validate_python(file_input_content, from_attributes=True)
+            _, content = await self.read(rfic.source.value.file_id, user_id=user_id)
         else:
             rfic, content = await self.add(file_input_content, user_id=user_id)
 
