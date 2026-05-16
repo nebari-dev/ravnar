4ac0d5310515093a5bb086f04f9422b45aea526a
Philip Meier <github.pmeier@posteo.de>
2026-05-16 22:09:50 +0200

    cleanup

--- src/_ravnar/api/__init__.py
+++ src/_ravnar/api/__init__.py
@@ -14,9 +14,43 @@ from .threads import make_router as make_threads_router
 
 if TYPE_CHECKING:
     from _ravnar.core import AgentHandler
-    from _ravnar.database import Database
-    from _ravnar.file_storage import FileHandler
-    from _ravnar.mixin import SetupTeardownMixin
+
+
+def make_router(
+    *,
+    storage_config: StorageConfig,
+    agent_handler: AgentHandler,
+    authenticated_user: Callable[..., Any],
+) -> schema.APIRouter:
+    router = schema.APIRouter(tags=["API"], dependencies=[Depends(authenticated_user)])
+
+    @router.get("/user")
+    async def get_user(
+        user: schema.User = Depends(authenticated_user),  # noqa: B008
+    ) -> schema.User:
+        return user
+
+    @router.get("/config")
+    async def get_config() -> schema.APIConfig:
+        return schema.APIConfig(
+            agents=agent_handler.configs,
+            storage_enabled=storage_config.enabled,
+        )
+
+    if storage_config.enabled:
+        router.include_router(
+            _make_stateful_router(
+                storage_config=storage_config,
+                agent_handler=agent_handler,
+                authenticated_user=authenticated_user,
+            )
+        )
+
+    router.include_router(
+        make_agents_router(agent_handler=agent_handler, authenticated_user=authenticated_user), prefix="/agents"
+    )
+
+    return router
 
 
 def _make_stateful_router(
@@ -49,41 +83,6 @@ def _make_stateful_router(
             authenticated_user=authenticated_user,
         ),
         prefix="/threads",
-    )
-
-    return router
-
-
-def make_router(
-    *,
-    storage_config: StorageConfig,
-    agent_handler: AgentHandler,
-    authenticated_user: Callable[..., Any],
-) -> schema.APIRouter:
-    router = schema.APIRouter(tags=["API"], dependencies=[Depends(authenticated_user)])
-
-    @router.get("/user")
-    async def get_user(
-        user: schema.User = Depends(authenticated_user),  # noqa: B008
-    ) -> schema.User:
-        return user
-
-    @router.get("/config")
-    async def get_config() -> schema.APIConfig:
-        return schema.APIConfig(
-            agents=agent_handler.configs,
-            storage_enabled=storage_config.enabled,
-        )
-
-    if storage_config.enabled:
-        router.include_router(_make_stateful_router(
-            storage_config=storage_config,
-            agent_handler=agent_handler,
-            authenticated_user=authenticated_user,
-        ))
-
-    router.include_router(
-        make_agents_router(agent_handler=agent_handler, authenticated_user=authenticated_user), prefix="/agents"
     )
 
     return router
